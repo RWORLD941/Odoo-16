5c437bd8f4d75b8e903cc9a1a11478e3f4db8a0e
RWORLD941 <160455329+RWORLD941@users.noreply.github.com>
2024-08-15 16:04:32 +0530

    Add files via upload

---
+++ custom/school_student/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    # App Information
+    'name': 'School Students',
+    'version': '1.1',
+    'category': 'Students',
+    'sequence': 1,
+
+    # Author
+    'author': 'Weblearns',
+    'maintainer': 'WebLearns',
+    'website': 'https://freeweblearns.blogspot.com',
+
+    # Dependencies
+    'depends': ['setu_college_management','school'],
+
+    'summary': "School Student Management System",
+
+    'support': 'support@weblearns.com',
+
+    'description': "This is school student management system software",
+
+    'data': [
+        'security/ir.model.access.csv',
+        "views/school_student_view.xml",
+        "views/setu_college_views.xml",
+             ],
+
+    # Technical
+
+}
